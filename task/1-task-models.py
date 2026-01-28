7590c2d92ab9a12a25540d73f36ec46e40f32ebd
Ivan Topchiyski <ivan_topchiiysky@epam.com>
2026-01-28 17:57:23 +0200

    task done

--- task/1-task-models.py
+++ task/1-task-models.py
@@ -11,8 +11,20 @@ from task.app.main import run
 # - claude-3-7-sonnet@20250219
 # - gemini-2.5-pro
 
+# run(
+#     deployment_name='gpt-4o',
+#     print_request=False, # Switch to False if you do not want to see the request in console
+#     print_only_content=False, # Switch to True if you want to see only content from response
+# )
+
+# run(
+#     deployment_name='claude-3-7-sonnet@20250219',
+#     print_request=False, # Switch to False if you do not want to see the request in console
+#     print_only_content=False, # Switch to True if you want to see only content from response
+# )
+
 run(
-    deployment_name='INSERT_DEPLOYMENT_NAME',
+    deployment_name='gemini-2.5-pro',
     print_request=False, # Switch to False if you do not want to see the request in console
     print_only_content=False, # Switch to True if you want to see only content from response
 )
